088315f55bff7f03176a219bbe2c9b229020b95c
gospelg <garrettcrosby@outlook.com>
2017-03-30 08:41:29 -0400

    Update 2.2
    First field tested proven working version. Added password update functionality.

--- google_import.py
+++ google_import.py
@@ -1,6 +1,6 @@
 #####################################################################################
 #                         Garrett's super duper google importer			    #
-#				  version 2.1					    #
+#				  version 2.2					    #
 #	Read me located on DC2 "C:\users\backend\google_import\readme.txt	    #
 #####################################################################################
 
@@ -9,8 +9,8 @@ from subprocess import call
 import time
 import logging
 
-date = time.strftime("%d_%m_%Y")
-logging.basicConfig(filename='C:\users\backend\desktop\google_import\logs\log{0}.log'.format(date), level=logging.DEBUG)
+date = time.strftime("%m_%d_%Y")
+logging.basicConfig(filename=r'C:\users\backend\desktop\google_import\logs\log{0}.log'.format(date), level=logging.DEBUG)
 
 #create a student object. All students have these several attributes, gleaned from the nefec csv
 class student(object):
@@ -27,12 +27,12 @@ class student(object):
 	def add_user(self):
 		#this is all the fields gam wants to create a new user
 		gam_switch = " ".join([self.email, self.firstname, self.lastname, self.password, self.s_id, self.grade])
-		#creates the user in google
+
 		try:
-			call("C:\Users\backend\Desktop\gam-64\gam.exe create user {0}".format(gam_switch))
-			logging.info("Created user {0} \n".format(gam_switch[0]))
+			call(r"C:\Users\backend\Desktop\gam-64\gam.exe create user {0}".format(gam_switch), shell=True)
+			logging.info("Created user {0}".format(gam_switch[0]))
 		except:
-			logging.warning("Failed to create user {0} \n".format(gam_switch[0]))
+			logging.warning("Failed to create user {0}".format(gam_switch[0]))
 
 	def move_user(self):
 		school = ""
@@ -45,21 +45,32 @@ class student(object):
 		group = school.replace(" ", "") + "@union.k12.fl.us"
 		try:
 			#calls gam to move user to correct OU
-			call('C:\Users\backend\Desktop\gam-64\gam.exe update org "/Chromebooks/Student/{0}" add users {1}'.format(school, self.email))
+			call(r'C:\Users\backend\Desktop\gam-64\gam.exe update org "/Chromebooks/Student/{0}" add users {1}'.format(school, self.email), shell=True)
 			#calls gam to add user to correct group
-			call("C:\Users\backend\Desktop\gam-64\gam.exe update group {0} add member user {1}".format(group, self.email))
-			logging.info("Sucessfully moved user {0} to new group and OU \n".format(self.email))
+			call(r"C:\Users\backend\Desktop\gam-64\gam.exe update group {0} add member user {1}".format(group, self.email), shell=True)
+			logging.info("Sucessfully moved user {0} to new group and OU".format(self.email))
 		except:
-			logging.warning("Could not move user {0} to new group or OU \n".format(self.email))
-
-#unsuspends account			
+			logging.warning("Could not move user {0} to new group or OU".format(self.email))
+		
 	def unsuspend(self):
 		try:
-			Call("C:\Users\backend\Desktop\gam-64\gam.exe update user {0} suspended off".format(self.email))
-			logging.info("Reactivated account for {0} \n".format(self.email))
+			Call(r"C:\Users\backend\Desktop\gam-64\gam.exe update user {0} suspended off".format(self.email), shell=True)
+			logging.info("Reactivated account for {0}".format(self.email))
 		except:
 			logging.warning("Could not reactivate account for {0}".format(self.email))
-	
+			
+	def change_password(self):
+		try:
+			call(r"C:\Users\backend\Desktop\gam-64\gam.exe update user {0} password {1}".format(self.email, self.password), shell=True)
+			logging.info("Successfully updated password for {0}".format(self.email))
+		except:
+			logging.warning("Could not update password for {0}".format(self.email))
+			
+def update_passwords(students):
+	for kid in students:
+		i = student(kid[2], kid[1], kid[0], kid[3], kid[11], kid[5], kid[4])
+		i.change_password()
+			
 #for item in nefec, item[11] is the student id. for x in google, x[3] is also student id, and x[1] is suspended, true or false.
 def new_students(nefec, google):
 	users_add = {"new":[], "unsuspend": []}
@@ -85,7 +96,6 @@ def gone_students(nefec, google):
 					break
 			else:
 				deactivate.append(student[0])
-				
 	return deactivate
 
 #arguement is a dictionary, as exported from new students function. This allows easy seperation of which students are new and which just need to have accounts unsuspended
@@ -103,12 +113,12 @@ def remove_students(students):
 	for kid in students:
 		try:
 			#suspends user. Did not do this through class, becasue the only attribute needed is email. Students will be a straightforward list of emails of kids to suspend. 
-			call("C:\Users\backend\Desktop\gam-64\gam.exe update user {0} suspended on".format(kid))
+			call(r"C:\Users\backend\Desktop\gam-64\gam.exe update user {0} suspended on".format(kid), shell=True)
 			#moves them to suspended users ou
-			call('C:\Users\backend\Desktop\gam-64\gam.exe update org "/Suspended Users" add users {0}'.format(kid)
-			logging.info("Sucessfully suspended user {0} and moved to suspended OU \n".format(kid))
+			call(r'C:\Users\backend\Desktop\gam-64\gam.exe update org "/Suspended Users" add users {0}'.format(kid), shell=True)
+			logging.info("Sucessfully suspended user {0} and moved to suspended OU".format(kid))
 		except:
-			logging.warning("Could not suspend {0} or move to suspended OU \n".format(kid))
+			logging.warning("Could not suspend {0} or move to suspended OU".format(kid))
 		
 #converts csv files to tuples  so they dont have to be reopened every time they need to be accessed. 	
 def list_maker(file_path):
@@ -127,13 +137,12 @@ def combiner(list1, list2):
 def main (): 
 	
 	#makes gam print out a csv of all the users in GAD, with all fields to a csv
-	call("C:\Users\backend\Desktop\gam-64\gam.exe print users suspended custom UCSDstudent > C:\users\backend\desktop\google_import\student_id.csv")
-	sleep(15)
+	call(r"C:\Users\backend\Desktop\gam-64\gam.exe print users suspended custom UCSDstudent > C:\users\backend\desktop\google_import\student_id.csv", shell=True)
 
 	#assign files to variables
-	logins = list_maker("C:\users\backend\desktop\google_import\sftp\GoogleLogins.csv")
-	home_logins = list_maker("C:\users\backend\desktop\google_import\sftp\GoogleLoginsHomeSchool.csv")
-	gam_output = list_maker("C:\users\backend\desktop\google_import\student_id.csv")
+	logins = list_maker(r"C:\users\backend\desktop\google_import\sftp\GoogleLogins.csv")
+	home_logins = list_maker(r"C:\users\backend\desktop\google_import\sftp\GoogleLoginsHomeSchool.csv")
+	gam_output = tuple(list_maker(r"C:\users\backend\desktop\google_import\student_id.csv"))
 	
 	logins = combiner(logins, home_logins)
 	
@@ -142,5 +151,6 @@ def main ():
 	
 	import_students(users_add)
 	remove_students(users_remove)
+	update_passwords(logins)
 	
 main()
