6c4e43d8636500a1f847a61689c5eb2e6b7c9b23
gospelg <garrettcrosby@outlook.com>
2018-08-07 09:40:07 -0400

    Update 4.0.0
    Now handles sftp operations natively, and adds quotations around student names before passing to gam, to prevent errors when dealing with names with special characters. Now fully utilizes setup file for common variables, and can be better customized for use in different organizations. Updated some notes within the source code.

--- google_import.py
+++ google_import.py
@@ -1,13 +1,15 @@
 #####################################################################################
 #                         Garrett's super duper google importer                     #
-#                                     version 3.1.3                                 #
-#         Read me located on DC2 "C:\users\backend\google_import\readme.txt         #
+#                                     version 4.0.0                                 #
+#         Read me located in addplication's root directory.                         #
 #####################################################################################
 
 import subprocess
 import csv
 import time
 import logging
+import paramiko as pm
+import ConfigParser
 
 """ The class basically has all the attributes of a student, and a few functions.
 The functions basically generate commands to pass to GAM. The put them into two txt
@@ -22,7 +24,7 @@ calls gam and points to these two files for the list of commands it should execu
 class student(object):
 
     def __init__(self, email, lastname, firstname, password,
-                 s_id, grade, department, master_file):
+                 s_id, grade, department, master_file, password_file):
         self.email = email
         self.lastname = lastname
         self.firstname = firstname
@@ -31,6 +33,7 @@ class student(object):
         self.grade = grade
         self.department = department
         self.master_file = master_file
+        self.password_file = password_file
 
     def add_user(self):
         #this is all the fields gam wants to create a new user
@@ -46,7 +49,10 @@ class student(object):
     def move_user(self):
         school = ""
         if self.department == "0031":
-            school = "LBES Students"
+            if self.grade == "KG" or "01" or "02":
+                school = "LBES Students/LBES Badge Login"
+            else:
+                school = "LBES Students"
         if self.department == "0022":
             school = "LBMS Students"
         if self.department == "0021":
@@ -58,12 +64,14 @@ class student(object):
                      .format(school, self.email))
         with open(self.master_file, 'a') as f:
             f.write(gam_input)
-        #ctells gam to add user to correct group
-        gam_input1 = ("gam update group {0} "
-                      "add member user {1}\n"
-                      .format(group, self.email))
-        with open(self.master_file, 'a') as f:
-            f.write(gam_input1)
+        #tells gam to add user to correct group
+        #there are no groups for elementary school kids, so exclude them
+        if school != "LBES Students":
+            gam_input1 = ("gam update group {0} "
+                          "add member user {1}\n"
+                          .format(group, self.email))
+            with open(self.master_file, 'a') as f:
+                f.write(gam_input1)
 
     def unsuspend(self):
         gam_input = ("gam update user {0} suspended off\n"
@@ -72,16 +80,16 @@ class student(object):
             f.write(gam_input)
             
     def change_password(self):
-        pass_file = r'C:\users\backend\desktop\google_import\passwords.txt'
+        password_file = self.password_file
         gam_input = ("gam update user {0} password {1}\n"
                      .format(self.email, self.password))
-        with open(pass_file, 'a') as f:
+        with open(password_file, 'a') as f:
             f.write(gam_input)
 
-def update_passwords(students):
+def update_passwords(students, passwords):
     for kid in students:
         i = student(kid[2], kid[1], kid[0], kid[3], kid[11],
-                    kid[5], kid[4], 0) #no need for the master_file atribute here
+                    kid[5], kid[4], 0, passwords) #no need for the master_file atribute here
         i.change_password()
 
 #item[11] is the student id, x[3] is also student id, and x[1] is suspended, true or false.
@@ -112,19 +120,20 @@ def gone_students(nefec, google):
     return deactivate
 
 #arguement is a dictionary, as exported from new students function.
+# 0 is a placeholder for password_file arg, which isn't needed here
 def import_students(students, master_file):
     for kid in students["new"]:
         i = student(kid[2], kid[1], kid[0], kid[3], kid[11], 
-                    kid[5], kid[4], master_file)
+                    kid[5], kid[4], master_file, 0)
         i.add_user()
         i.move_user()
     for kid in students["unsuspend"]:
         i = student(kid[2], kid[1], kid[0], kid[3], kid[11], 
-                    kid[5], kid[4], master_file)
+                    kid[5], kid[4], master_file, 0)
         i.unsuspend()
         i.move_user()
 
-#suspends user. Did not do this through class, becasue the only attribute needed is email.
+#suspends user. Did not do this through class, because the only attribute needed is email.
 def remove_students(students, master_file):
     for kid in students:
         gam_input = ("gam update user {0} suspended on\n"
@@ -138,11 +147,15 @@ def remove_students(students, master_file):
             f.write(gam_input1)
 
 #converts csv files to tuples  so they dont have to be reopened every time they need to be accessed.
+#add double quotes to first name and last name indexes so special characters dont make gam freak
 def list_maker(file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f, delimiter=',')
         next(reader, None)
         new_list = list(reader)
+        for x in new_list:
+            x[0] = r'"{0}"'.format(x[0])
+            x[1] = r'"{0}"'.format(x[1])
         return new_list
 
 #combines homeschool logins with regular logins and turns them into one tuple
@@ -151,8 +164,7 @@ def combiner(list1, list2):
         list1.append(item)
     return tuple(list1)
 
-def gam_master(master_file):
-    pass_file = r'C:\users\backend\desktop\google_import\passwords.txt'
+def gam_master(master_file, pass_file):
     #make sure gam runs these commands one at a time, in order
     try:
         subprocess.call('set GAM_THREADS=1', shell=True)
@@ -190,26 +202,84 @@ def gam_master(master_file):
     cleanup = open(pass_file, 'w')
     cleanup.close
 
-def main ():
+def sftp_ops(root_dir, date):
+    #get sftp connection parameters from config file
+    Config = ConfigParser.ConfigParser()
+    Config.read('setup.ini')
+    host = Config.get('SFTP', 'Server')
+    port = 22
+    user = Config.get('SFTP', 'Username')
+    password = Config.get('SFTP', 'Password')
 
-    root_dir = r'C:\users\backend\desktop\google_import'
+    connections = (
+    ['/Data/googleLogins/GoogleLogins.csv', 
+    '{0}\sftp\GoogleLogins.csv'.format(root_dir)],
+    ['/Data/googleLogins/GoogleLoginsHomeSchool.csv', 
+    '{0}\sftp\GoogleLoginsHomeSchool.csv'.format(root_dir)],
+    ['/Data/lanSchool/ClassesByTeacherLoginName.csv', 
+    'C:\lanschool\ClassesByTeacherLoginName.csv'],
+    ['/Data/lanSchool/StudentsForClassByLoginName.csv', 
+    'C:\lanschool\StudentsForClassByLoginName.csv'],
+    )
+    
+    #set up sftp log
+    pm.util.log_to_file("{0}_sftp_log")
+    
+    transport = pm.Transport((host, port))
+    transport.connect(username=user, password=password)
+    sftp = pm.SFTPClient.from_transport(transport)
+    
+    #it is now connected, begin operations
+    #item[0] translates to remote path, item[1] is local path
+    for item in connections:
+        sftp.get(item[0], item[1])    
+    sftp.close()
+    
+    
+    
+def main ():
+    
     date = time.strftime("%m_%d_%Y")
-    logging.basicConfig(filename='{0}\logs\log{1}.log'
+    Config = ConfigParser.ConfigParser()
+    Config.read('setup.ini')
+    root_dir = Config.get('General', 'AppRootDir')
+    master_file = Config.get('General', 'MasterFile') + (
+    'master{0}.txt'.format(date))
+    password_file = Config.get('General', 'PasswordFile') + (
+    'passwordfile.txt')
+    
+    
+    #ensure gam.exe is in the windows PATH var, if not figure out the path
+    if Config.get('General', 'gamisinpath') == 'True':
+        gam = 'gam'
+    elif Config.has_option('General', 'AltGamPath'):
+        gam = Config.get('General', 'AltGamPath')   
+    else:
+        gam = raw_input('Please specify the path to gam.exe \n')
+        Config.set('General', 'AltGamPath', gam)
+    
+    #set up a log file, with the current date. Debug level catches info, err, and warning.
+    logging.basicConfig(filename ='{0}\\logs\\log{1}.log'
                         .format(root_dir, date), level=logging.DEBUG)
-    master_file = r"{0}\master\{1}master.txt".format(root_dir, date)
+    
 
     #makes gam print out a csv of all the users in GAD, with all fields to a csv
     logging.info("Exporting users from google...")
-    gam_sheet = (r"C:\Users\backend\Desktop\gam-64\gam.exe "
-                 r"print users suspended custom UCSDstudent "
-                 r"> C:\users\backend\desktop\google_import\student_id.csv")
+    gam_sheet = ('{0} print users suspended custom UCSDstudent '
+                 '> {1}\student_id.csv'.format(gam, root_dir))
     subprocess.call(gam_sheet, shell=True)
-
+    
+    #perform SFTP operations
+    try:
+        sftp_ops(root_dir, date)
+    except:
+        logging.warning('SFTP OPS failed, check paramiko log.')
+        
     #assign files to variables
     logging.info("Making lists...")
-    logins = list_maker((r"{0}\sftp\GoogleLogins.csv").format(root_dir))
-    home_logins = list_maker((r"{0}\sftp\GoogleLoginsHomeSchool.csv").format(root_dir))
-    gam_output = tuple(list_maker((r"{0}\student_id.csv").format(root_dir)))
+    logins = list_maker(('{0}\\sftp\\GoogleLogins.csv').format(root_dir))
+    home_logins = list_maker(('{0}\\sftp\\GoogleLoginsHomeSchool.csv').format(root_dir))
+    gam_output = tuple(list_maker(('{0}\\student_id.csv').format(root_dir)))
 
     logins = combiner(logins, home_logins)
 
@@ -218,9 +288,10 @@ def main ():
 
     import_students(users_add, master_file)
     remove_students(users_remove, master_file)
-    update_passwords(logins)
+    #update_passwords(logins, password_file)
     
     logging.info("Staring batch operations...")
-    gam_master(master_file)
+    gam_master(master_file, password_file)
+    
 
 main()
